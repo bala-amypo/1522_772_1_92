032fa7e68bacb5b4474328df4cc832ee153c1403
bala-amypo <bala@amypo.in>
2025-12-18 17:44:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/AnalysisLogEntity.java
+++ src/main/java/com/example/demo/entity/AnalysisLogEntity.java
@@ -1,54 +1,25 @@
-package com.example.collectiondp.entity;
+package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 @Entity
 public class AnalysisLogEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
+
     private String message;
 
     @ManyToOne
     private HotspotZoneEntity zone;
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
 
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public HotspotZoneEntity getZone() {
-        return zone;
-    }
-
-    public void setZone(HotspotZoneEntity zone) {
-        this.zone = zone;
-    }
-
-    public AnalysisLogEntity() {
-    }
-
-    public AnalysisLogEntity(Long id, String message, HotspotZoneEntity zone) {
-        this.id = id;
-        this.message = message;
-        this.zone = zone;
-    }
-
-    
+    public HotspotZoneEntity getZone() { return zone; }
+    public void setZone(HotspotZoneEntity zone) { this.zone = zone; }
 }
