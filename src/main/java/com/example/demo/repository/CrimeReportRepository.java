6f7744da6c39b429c3cf0fdc7cd08699cda8766b
bala-amypo <bala@amypo.in>
2025-12-20 06:28:22 +0000

    User commit

--- src/main/java/com/example/demo/repository/CrimeReportRepository.java
+++ src/main/java/com/example/demo/repository/CrimeReportRepository.java
@@ -13,15 +13,3 @@ public interface CrimeReportRepository
             double minLong, double maxLong
     );
 }
-package com.example.demo.repository;
-
-import com.example.demo.model.CrimeReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import java.util.List;
-
-public interface CrimeReportRepository extends JpaRepository<CrimeReport, Long> {
-
-    @Query("SELECT c FROM CrimeReport c WHERE c.latitude BETWEEN :minLat AND :maxLat AND c.longitude BETWEEN :minLong AND :maxLong")
-    List<CrimeReport> findByLatLongRange(double minLat, double maxLat, double minLong, double maxLong);
-}
