93232870fbf7f56af35be74646725e293623c580
bala-amypo <bala@amypo.in>
2025-12-25 10:25:44 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtUtil.java
+++ src/main/java/com/example/demo/config/JwtUtil.java
@@ -3,18 +3,19 @@ package com.example.demo.config;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+@Component
 public class JwtUtil {
 
     private final String secret =
             "9a4f2c8d3b7a1e6f45c8a0b3f267d8b1d4e6f3c8a9d2b5f8e3a9c8b5f6d7a4b2";
 
-    private final long expiration = 86400000; // 24 hours
+    private final long expiration = 86400000;
 
     public String generateToken(Long userId, String email, String role) {
-
         return Jwts.builder()
                 .claim("userId", userId)
                 .claim("email", email)
